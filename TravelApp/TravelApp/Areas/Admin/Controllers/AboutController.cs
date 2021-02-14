8727601630ebf00362fa48c3afaf92fbdecf9e59
terlan427 <terlan427@gmail.com>
2021-02-14 09:20:29 -0800

    About was maked

--- TravelApp/TravelApp/Areas/Admin/Controllers/AboutController.cs
+++ TravelApp/TravelApp/Areas/Admin/Controllers/AboutController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TravelApp.Areas.Admin.Models;
@@ -152,5 +153,6 @@ namespace TravelApp.Areas.Admin.Controllers
                 });
             }
         }
+
     }
 }
--- TravelApp/TravelApp/Areas/Admin/Controllers/PhotoController.cs
+++ TravelApp/TravelApp/Areas/Admin/Controllers/PhotoController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using TravelApp.Data;
 
 namespace TravelApp.Areas.Admin.Controllers
@@ -14,9 +15,14 @@ namespace TravelApp.Areas.Admin.Controllers
     [Authorize]
     public class PhotoController : Controller
     {
-        public async Task<JsonResult> Upload(List<IFormFile> Photos,string folder)
+        private readonly IConfiguration _configuration;
+        public PhotoController(IConfiguration configuration)
         {
-            if(folder!=null && Photos.Count != 0 && Photos != null)
+            _configuration = configuration;
+        }
+        public async Task<JsonResult> Upload(List<IFormFile> Photos, string folder)
+        {
+            if (folder != null && Photos.Count != 0 && Photos != null)
             {
                 foreach (IFormFile file in Photos)
                 {
@@ -28,7 +34,7 @@ namespace TravelApp.Areas.Admin.Controllers
                         newPath = path.Replace(file.FileName, guid + file.FileName);
                     }
 
-                    using (FileStream fs = new FileStream(newPath!=null?newPath:path, FileMode.Create))
+                    using (FileStream fs = new FileStream(newPath != null ? newPath : path, FileMode.Create))
                     {
                         await file.CopyToAsync(fs);
                     }
@@ -41,8 +47,54 @@ namespace TravelApp.Areas.Admin.Controllers
             }
             else
             {
-                return Json(new{
+                return Json(new
+                {
                     status = 400
+                });
+
+            }
+        }
+
+        public async Task<JsonResult> UploadCkEditorPhoto(List<IFormFile> upload, string folder)
+        {
+            if (folder != null && upload.Count != 0 && upload != null)
+            {
+                string fileName = "";
+                string newPath = null;
+                string path = "";
+                foreach (IFormFile file in upload)
+                {
+                     path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Uploads", folder, file.FileName);
+                    fileName = file.FileName;
+                    if (System.IO.File.Exists(path))
+                    {
+                        Guid guid = Guid.NewGuid();
+                        fileName = guid + file.FileName;
+                        newPath = path.Replace(file.FileName, fileName);
+                    }
+
+                    using (FileStream fs = new FileStream(newPath != null ? newPath : path, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fs);
+                    }
+
+
+                }
+
+                var filePath = _configuration["FileUrl"] +folder+"/"+ fileName;
+                return Json(new
+                {
+                    uploaded = 1,
+                    fileName = fileName,
+                    url = filePath
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    uploaded = 0,
+                    error = new Dictionary<string, string>() { { "message","Xəta baş verdi."} }
                 });
 
             }
